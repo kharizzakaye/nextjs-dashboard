8fd2452f94b51449c6528bc2a5b06bc034b93e5c
Khariza <kharizakaye@gmail.com>
2025-05-06 19:20:23 +0800

    Initial commit

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -52,7 +52,7 @@ export type InvoicesTable = {
   image_url: string;
   date: string;
   amount: number;
-  status: 'pending' | 'paid';
+  status: 'pending' | 'paid'; // String union type
 };
 
 export type CustomersTableType = {
